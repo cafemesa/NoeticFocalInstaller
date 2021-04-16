dd220260ee060a74b2d0435b2520981da802f0f1
cafemesa <c.f.medina@hotmail.com>
2021-04-16 12:35:58 +0200

    Update ROS_Noetic_Install.sh

--- ROS_Noetic_Install.sh
+++ ROS_Noetic_Install.sh
@@ -4,17 +4,22 @@ sudo add-apt-repository universe
 sudo add-apt-repository multiverse
 sudo add-apt-repository restricted
 sudo apt update & sudo apt upgrade
+
 ## Setup your sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
 ## Set up your keys
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
 ## Installation
 sudo apt update
 sudo apt install ros-noetic-desktop-full
+
 ## Environment setup
 rossource="source /opt/ros/melodic/setup.bash"
 echo "$rossource" >> ~/.bashrc
 eval $rossource
+
 ## Dependencies for building packages
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 sudo rosdep init
